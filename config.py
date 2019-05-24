73c32cc327f00629c52c31b8f3a5a52dfe3fc9c6
Toby Yang <tianer2820@163.com>
2019-05-24 09:08:51 +0800

    added config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# this is the config file for the program
+
+# your team number
+TEAM = 5451
+
+# leave it default to use the default roboRIO ip
+# or set a custom one, like '127.0.0.1'
+SERVER_IP = 'default'
+
